15bc70b8acb45fce9e25226de44442b9ff953000
chilusoft <mukukachilu@gmail.com>
2023-06-27 15:29:06 +0200

    Added realtime graph UI

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,16 @@
 # chat/consumers.py
 import json
-
+import random
+import time
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
-
+from .models import ChannelName
+from channels.consumer import SyncConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        # async_to_sync(ChannelName.objects.create(channel_name=self.channel_name, channel_description=self.room_name))
         self.room_group_name = "chat_%s" % self.room_name
 
         # Join room group
@@ -34,3 +38,24 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+class DataWidgetConsumer(SyncConsumer):
+
+    def websocket_connect(self, event):
+        self.send({
+            "type": "websocket.accept",
+        })
+        print(event)
+
+    def websocket_receive(self, event):
+        for i in range(20):
+
+            time.sleep(1)
+            self.send({
+                "type": "websocket.send",
+                "text": f'{random.randint(1, 10)}',
+            })
+        # print(event)
+    def websocket_disconnect(self, event):
+        # print(event)
+        pass
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ChannelName(models.Model):
+    channel_name = models.CharField(max_length=100, null=True, blank=True)
+    channel_description = models.CharField(max_length=100, null=True, blank=True)
+    # channel_image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.channel_name
